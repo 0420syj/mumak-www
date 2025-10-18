740c911f0956dd5e034be61c1990287cf4592920
Wan Sim <0420syj@naver.com>
2025-10-18 16:40:33 +0900

    chore: 불필요 eslint rule 비활성화

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -44,6 +44,8 @@ export const nextJsConfig = [
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
       'react/react-in-jsx-scope': 'off',
+      // Disable triple-slash reference for next-env.d.ts
+      '@typescript-eslint/triple-slash-reference': 'off',
     },
   },
   {
