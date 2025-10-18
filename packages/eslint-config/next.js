4c4c372f7c8dcf2e1941e5df75d81e039d4471c9
Wan Sim <0420syj@naver.com>
2025-10-18 14:32:13 +0900

    chore: 작업 내용 커밋

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginReact from 'eslint-plugin-react';
-import globals from 'globals';
 import pluginNext from '@next/eslint-plugin-next';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import { config as baseConfig } from './base.js';
 
 /**
@@ -45,5 +45,18 @@ export const nextJsConfig = [
       // React scope no longer necessary with new JSX transform.
       'react/react-in-jsx-scope': 'off',
     },
+  },
+  {
+    ignores: [
+      'dist/**',
+      'out/**',
+      '.next/**',
+      'build/**',
+      'coverage/**',
+      'test-results/**',
+      'playwright-report/**',
+      'node_modules/**',
+      '*.tsbuildinfo',
+    ],
   },
 ];
